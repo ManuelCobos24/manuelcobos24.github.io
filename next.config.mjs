061cac3014119a89c283a34bd134781bb1d75574
Manuel Cobos Solís <manuelcobos200324@gmail.com>
2026-04-21 13:23:01 +0200

    feat: add Next.js deployment workflow and update configuration for GitHub Pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,22 @@
 /** @type {import('next').NextConfig} */
+
+const isGithubPages = process.env.GITHUB_ACTIONS === 'true'
+
+const repoName = 'manuelcobos24.github.io'
+
 const nextConfig = {
   output: 'export',
+
   typescript: {
     ignoreBuildErrors: true,
   },
+
   images: {
     unoptimized: true,
   },
+
+  basePath: isGithubPages ? `/${repoName}` : '',
+  assetPrefix: isGithubPages ? `/${repoName}/` : '',
 }
 
 export default nextConfig
